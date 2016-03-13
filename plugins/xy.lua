b26b2186064e7efdc8bf62d6b4c4bd6ac51327c6
parsaalemi <pa.alemi81@gmail.com>
2016-03-13 12:28:44 +0330

    Create xy.lua

---
+++ plugins/xy.lua
@@ -0,0 +1,16 @@
+do
+local function run(msg, matches)
+local bot_id = our_id 
+local fbot1 = 105831687
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
